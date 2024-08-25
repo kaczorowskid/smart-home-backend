4f006d0c1afff282f0c0355c0944424f2ae18401
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-08-25 13:34:30 +0200

    feat: remove displayed-device module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { DatabaseModule } from './database/database.module';
 import { DevicesModule } from './devices/devices.module';
-import { DisplayedDeviceModule } from './displayed-device/displayed-device.module';
-import { DisplayedDeviceService } from './displayed-device/displayed-device.service';
-import { DisplayedDeviceController } from './displayed-device/displayed-device.controller';
 
 @Module({
   imports: [
@@ -19,9 +16,6 @@ import { DisplayedDeviceController } from './displayed-device/displayed-device.c
     ConfigModule.forRoot(),
     DatabaseModule,
     DevicesModule,
-    DisplayedDeviceModule,
   ],
-  providers: [DisplayedDeviceService],
-  controllers: [DisplayedDeviceController],
 })
 export class AppModule {}
