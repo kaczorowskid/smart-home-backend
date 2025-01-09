7f2a7d66595b41ee86f3ee71113a68a071bea0fa
Damian Kaczorowski <kaczorowskid97@gmail.com>
2025-01-09 11:40:27 +0100

    fix: fix the seeder

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -1,26 +1,44 @@
 import { PrismaClient } from '@prisma/client';
+import { PermissionType } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 const main = async () => {
-  // const admin = await prisma.user.create({
-  //   data: {
-  //     name: 'Test',
-  //     surname: 'User',
-  //     email: 'test@user.com',
-  //     password: '$2a$10$ZDQWHBF1k4XFuDsQa2n39OEr/eSpsJXF.8LRkDAdw/bNHAC3u6Da2', //TestPassword!234,
-  //     isVerified: true,
-  //     role: 'ADMIN',
-  //   },
-  // });
-  // console.log(admin);
+  try {
+    await prisma.$transaction(async (transaction) => {
+      const role = await transaction.role.create({
+        data: {
+          name: 'ADMIN',
+          id: '7ea0d03c-74d6-4d52-afd6-e314a5931554',
+          permissions: {
+            create: {
+              permission: PermissionType.IS_ADMIN,
+              id: 'b63a6f88-c690-454c-b4df-b48c01c833a9',
+            },
+          },
+        },
+      });
+
+      await transaction.user.create({
+        data: {
+          name: 'Super',
+          roleId: role.id,
+          surname: 'Admin',
+          isVerified: true,
+          email: 'super@admin.com',
+          id: 'b60211e0-71cf-43b0-b62b-fb0fb7aa4f6a',
+          password:
+            '$2a$10$ZDQWHBF1k4XFuDsQa2n39OEr/eSpsJXF.8LRkDAdw/bNHAC3u6Da2', // TestPassword!234,
+        },
+      });
+    });
+
+    console.log('Seed completed successfully');
+  } catch (error) {
+    console.error('Transaction failed:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+main();
